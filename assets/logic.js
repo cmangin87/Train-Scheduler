3e6faad8649b858d695c54aa373595a94c6865f4
Christopher Mangin <mangin.christopher@gmail.com>
2019-10-09 11:18:43 -0400

    some additions, form still does not reset

--- assets/logic.js
+++ assets/logic.js
@@ -1,3 +1,5 @@
+displayTime();
+
 var config = {
     apiKey: "AIzaSyDXLrPyvKU22E2pTYhcdLO4Y0anyMKgEWk",
     authDomain: "train-schedule2-28d63.firebaseapp.com",
@@ -6,77 +8,86 @@ var config = {
     storageBucket: "",
     messagingSenderId: "368704588750",
     appId: "1:368704588750:web:117babbef9d0045208799f"
-  };
-  
-  firebase.initializeApp(config);
-  
-    var database = firebase.database();
+};
 
-        var trainName = "";
-        var destination = "";
-        var trainTime = "";
-        var frequency = 0;
+firebase.initializeApp(config);
 
-  $("#submitBtn").on("click", function (event) {
-      event.preventDefault();
+var database = firebase.database();
+
+var trainName = "";
+var destination = "";
+var trainTime = "";
+var frequency = 0;
+
+$("#submitBtn").on("click", function (event) {
+    event.preventDefault();
 
     trainName = $("#trainNameInput").val().trim();
     destination = $("#destinationInput").val().trim();
     trainTime = $("#trainTimeInput").val().trim();
     frequency = $("#frequencyInput").val().trim();
 
-        console.log(trainName);
-        console.log(destination);
-        console.log(trainTime);
-        console.log(frequency);
+    console.log(trainName);
+    console.log(destination);
+    console.log(trainTime);
+    console.log(frequency);
 
-        database.ref().push({
-            trainName: trainName,
-            destination: destination,
-            trainTime: trainTime,
-            frequency: frequency,
-            dateAdded: firebase.database.ServerValue.TIMESTAMP
-        })
-  })
+    database.ref().push({
+        trainName: trainName,
+        destination: destination,
+        trainTime: trainTime,
+        frequency: frequency,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+    })
 
-  database.ref().on("child_added", function (childSnapshot) {
-      console.log(childSnapshot.val());
-      console.log(childSnapshot.val().trainName);
-      console.log(childSnapshot.val().destination);
-      console.log(childSnapshot.val().trainTime);
-      console.log(childSnapshot.val().frequency);
-   
-        trainTime = childSnapshot.val().trainTime;
-        frequency = childSnapshot.val().frequency;
+})
 
-        var trainTimeConverted = moment(trainTime, "HH:mm").subtract(1, "years");
-            console.log(trainTimeConverted);
-            
-        var currentTime = moment();
-            console.log("Current Time: " + moment(currentTime).format("HH:mm"));
+database.ref().on("child_added", function (childSnapshot) {
+    console.log(childSnapshot.val());
+    console.log(childSnapshot.val().trainName);
+    console.log(childSnapshot.val().destination);
+    console.log(childSnapshot.val().trainTime);
+    console.log(childSnapshot.val().frequency);
 
-        var diffTime = moment().diff(moment(trainTimeConverted), "minutes");
-            console.log("Difference in Time: " + diffTime);
+    trainTime = childSnapshot.val().trainTime;
+    frequency = childSnapshot.val().frequency;
 
-        var timeRemainder = diffTime % frequency;
-            console.log(timeRemainder);
+    var trainTimeConverted = moment(trainTime, "HH:mm").subtract(1, "years");
+    console.log(trainTimeConverted);
 
-        var minutesUntil = frequency - timeRemainder;
-            console.log("Minutes Until Train Arrives: " + minutesUntil);
+    var currentTime = moment();
+    console.log("Current Time: " + moment(currentTime).format("HH:mm"));
 
-        var nextTrain = moment().add(minutesUntil, "minutes");
-            console.log("Arriving at: " + moment(nextTrain).format("HH:mm"));
+    var diffTime = moment().diff(moment(trainTimeConverted), "minutes");
+    console.log("Difference in Time: " + diffTime);
+
+    var timeRemainder = diffTime % frequency;
+    console.log(timeRemainder);
+
+    var minutesUntil = frequency - timeRemainder;
+    console.log("Minutes Until Train Arrives: " + minutesUntil);
+
+    var nextTrain = moment().add(minutesUntil, "minutes");
+    console.log("Arriving at: " + moment(nextTrain).format("HH:mm"));
 
 
-            $("#new-train").append(" <tr class='display'><td id='train-display'> " + childSnapshot.val().trainTime +
-                " </td><td id='destination-display'> " + childSnapshot.val().destination +
-                " </td><td id='frequency-display'> " + childSnapshot.val().frequency +
-                " </td><td id='time-display'> " + moment(nextTrain).format("HH:mm") + 
-                " </td> + <td id='arrival'> " + minutesUntil + "</tr>");
+    $("#new-train").append(" <tr class='display'><td id='train-display'> " + childSnapshot.val().trainName +
+        " </td><td id='destination-display'> " + childSnapshot.val().destination +
+        " </td><td id='frequency-display'> " + childSnapshot.val().frequency +
+        " </td><td id='time-display'> " + moment(nextTrain).format("HH:mm") +
+        " </td> + <td id='arrival'> " + minutesUntil + "</tr>");
 
-  }, function(errorObject) {
+}, function (errorObject) {
 
-            console.log("Errors handled: " + errorObject.code);
-        });
+    console.log("Errors handled: " + errorObject.code);
+});
 
-   
+
+
+function displayTime() {
+
+    var time = moment().format('HH:mm:ss');
+    $(".lead").html("Local Time: " + time);
+    setTimeout(displayTime, 1000);
+
+}
